e05aa47c6d2ee206ba3e3ba04e36f33376b27a52
Lorman Lau <lormanlau@gmail.com>
2021-08-06 09:49:49 -0700

    fixed mongoose config and added gitignore

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const port = 8000;
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 
--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const db_name = "quote_db"
+const db_name = "quote_db";
 
-mongoose.connect('mongodb://localhost/${db_name}', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Established a connection to the database'))
-    .catch(err => console.log('Something went wrong when connecting to the database ', err));
+mongoose
+  .connect(`mongodb://localhost/${db_name}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Established a connection to the database"))
+  .catch((err) =>
+    console.log("Something went wrong when connecting to the database ", err)
+  );
